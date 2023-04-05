bb79ad4eafc5731a307dd364c379c03a06a2670c
Yuto Yasuda <yuto.yasuda@rakus-partners.co.jp>
2023-04-05 15:02:29 +0900

    postデザイン完成 in feature/post

--- src/components/timeline/TimeLine.tsx
+++ src/components/timeline/TimeLine.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import "./TimeLine.css";
 import Share from "../share/Share";
+import Post from "../post/Post";
 
 export default function TimeLine() {
   return (
     <div className="timeline">
       <div className="timelineWrapper">
         <Share />
-        {/* <Post /> */}
+        <Post />
+        <Post />
+        <Post />
+        <Post />
       </div>
     </div>
   );
