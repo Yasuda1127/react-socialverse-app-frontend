55166d74fe7ba698f1ac0e3b596784dbbb578f0e
Yuto Yasuda <yuto.yasuda@rakus-partners.co.jp>
2023-04-11 20:20:31 +0900

    タイムライン取得 in feature/connect

--- src/components/timeline/TimeLine.tsx
+++ src/components/timeline/TimeLine.tsx
@@ -1,26 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./TimeLine.css";
 import Share from "../share/Share";
 import Post from "../post/Post";
-import { Posts } from "../../dummyData";
+import axios, { AxiosResponse } from "axios";
+// import { Posts } from "../../dummyData";
 
-// type PostData = {
-//   id: number;
-//   desc: string;
-//   photo: string;
-//   date: string;
-//   userId: number;
-//   like: number;
-//   comment: number;
-// };
+type PostData = {
+  id: number;
+  desc: string;
+  photo: string;
+  date: string;
+  userId: number;
+  like: number;
+  comment: number;
+};
 
 export default function TimeLine() {
+  const [posts, setPosts] = useState<PostData[]>([]);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response: AxiosResponse<PostData[]> = await axios.get(
+        "/posts/timeline/6423a3c906b3bd8e515d67ab"
+      );
+      console.log(response);
+      setPosts(response.data)
+    };
+    fetchPosts();
+  }, []); // 一度だけタイムラインを読み込むための、useEffect。第二引数を空にすると、中に書いたものが一度だけ読み込まれる。
+
   return (
     <div className="timeline">
       <div className="timelineWrapper">
         <Share />
-        {Posts.map((post) => (
-          <Post post={post} key={post.id} />
+        {posts.map((post) => ( // postsは自分の投稿と自分がフォローしているユーザーの投稿内容全てを含んだもの。
+          <Post post={post} key={post.id} /> 
         ))}
       </div>
     </div>
